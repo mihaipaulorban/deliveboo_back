f2c0f863388da495fb6d46e5c0ab385f2856cb76
danielecarpentiero <carpentierodaniele@gmail.com>
2024-03-01 15:41:55 +0100

    feat: foods table

---
+++ database/migrations/2024_03_01_142615_create_foods_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('foods', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('img')->nullable();
+            $table->text('description');
+            $table->decimal('price', 5, 2);
+            $table->boolean('is_visible')->default(1);
+            $table->boolean('is_vegetarian')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('foods');
+    }
+};
