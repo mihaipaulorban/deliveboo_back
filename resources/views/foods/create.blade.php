8e209388ecbf417daf59498f924a9c41fbc82814
DaviGel <geloso.davide@gmail.com>
2024-03-04 16:48:25 +0100

    feat: create foods

--- resources/views/foods/create.blade.php
+++ resources/views/foods/create.blade.php
@@ -2,42 +2,72 @@
 
 @section('content')
     @include('partials.errors')
+    {{-- Aggiungere errori dopo ogni singolo campo di input --}}
     <div class="container">
         <form action="{{ route('admin.foods.store') }}" method="POST" enctype="multipart/form-data">
             @csrf
             <h1 class="text-center">Create a new food</h1>
             <div class="mb-3">
                 <label class="form-label">
-                    <h2>Food title</h2>
+                    <h2>Food Name</h2>
                 </label>
-                <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                @error('name')
+                    <div class="alert alert-danger mt-1">{{ $message }}</div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label class="form-label">
                     <h3>Food Description</h3>
                 </label>
                 <textarea class="form-control" rows="3" name="description">{{ old('description') }}</textarea>
+                @error('description')
+                    <div class="alert alert-danger mt-1">{{ $message }}</div>
+                @enderror
             </div>
             <!-- Campo per l'immagine -->
             <div class="mb-3">
                 <label class="form-label">
                     <h3>Food Image</h3>
                 </label>
-                <input type="file" class="form-control" name="image" accept="image/*" required>
+                <input class="form-control" type="file" id="img" name="img">
+                @error('img')
+                    <div class="alert alert-danger mt-1">{{ $message }}</div>
+                @enderror
             </div>
-            <!-- Radio button per indicare se l'utente è vegetariano -->
+            <!-- Radio button per indicare se il food è vegetariano -->
             <div class="mb-3">
                 <label class="form-label">
-                    <h3>Are you vegetarian?</h3>
+                    <h3>Is the dish vegetarian?</h3>
                 </label>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="is_vegetarian" id="vegetarian_yes" value="1" checked>
+                    <input class="form-check-input" type="radio" name="is_vegetarian" id="vegetarian_yes" value="1">
                     <label class="form-check-label" for="vegetarian_yes">Yes</label>
                 </div>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="is_vegetarian" id="vegetarian_no" value="0">
+                    <input class="form-check-input" type="radio" name="is_vegetarian" id="vegetarian_no" value="0" checked>
                     <label class="form-check-label" for="vegetarian_no">No</label>
                 </div>
+                @error('is_vegetarian')
+                <div class="alert alert-danger mt-1">{{ $message }}</div>
+                @enderror
+            </div>
+            <!-- Radio button per indicare se il piatto è visibile -->
+            <div class="mb-3">
+                <label class="form-label">
+                    <h3>Is the dish visible?</h3>
+                </label>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="is_visible" id="visible_yes" value="1" checked>
+                    <label class="form-check-label" for="vegetarian_yes">Yes</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="is_visible" id="visible_yes" value="0">
+                    <label class="form-check-label" for="vegetarian_no">No</label>
+                </div>
+                @error('is_visible')
+                <div class="alert alert-danger mt-1">{{ $message }}</div>
+                @enderror
             </div>
             <!-- Campo per inserire il prezzo -->
             <div class="mb-3">
@@ -45,6 +75,9 @@
                     <h3>Price</h3>
                 </label>
                 <input type="number" class="form-control" name="price" value="{{ old('price') }}" required>
+                @error('price')
+                    <div class="alert alert-danger mt-1">{{ $message }}</div>
+                @enderror
             </div>
             <button type="submit" class="btn btn-success">Submit</button>
         </form>
