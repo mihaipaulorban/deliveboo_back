bb19810772cd418be19959e53e206aa6043985a8
lorenzo91347 <lorenzo.losito91@gmail.com>
2024-03-04 17:10:40 +0100

    feat:error alerts in create restaurants

--- resources/views/restaurants/create.blade.php
+++ resources/views/restaurants/create.blade.php
@@ -12,12 +12,18 @@
                     <h2>Restaurant name</h2>
                 </label>
                 <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                @error('name')
+                    <div class="alert alert-danger mt-1">{{ $message }}</div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label class="form-label">
                     <h3>Restaurant Address</h3>
                 </label>
                 <input type="text" class="form-control" name="address" value="{{ old('address') }}">
+                @error('address')
+                    <div class="alert alert-danger mt-1">{{ $message }}</div>
+                @enderror
             </div>
             <!-- Campo per l'immagine -->
             <div class="mb-3">
@@ -25,6 +31,9 @@
                     <h3>Restaurant Image</h3>
                 </label>
                 <input type="file" class="form-control" name="logo" required>
+                @error('logo')
+                    <div class="alert alert-danger mt-1">{{ $message }}</div>
+                @enderror
             </div>
             <!-- Campo per l'immagine -->
             <div class="mb-3">
@@ -32,6 +41,9 @@
                     <h3>Restaurant Cover Image</h3>
                 </label>
                 <input type="file" class="form-control" name="cover_img" required>
+                @error('cover_img')
+                    <div class="alert alert-danger mt-1">{{ $message }}</div>
+                @enderror
             </div>
             <!-- Campo P. Iva -->
             <div class="mb-3">
@@ -39,6 +51,9 @@
                     <h2>Restaurant P. Iva</h2>
                 </label>
                 <input type="text" class="form-control" name="p_iva" value="{{ old('p_iva') }}">
+                @error('p_iva')
+                    <div class="alert alert-danger mt-1">{{ $message }}</div>
+                @enderror
             </div>
             <button type="submit" class="btn btn-success">Submit</button>
         </form>
