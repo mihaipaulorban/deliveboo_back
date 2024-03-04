21e2e341ecebac8f7f539c4a3977fdf26de152c1
danielecarpentiero <carpentierodaniele@gmail.com>
2024-03-04 10:32:11 +0100

    feat: foodcontroller

--- routes/web.php
+++ routes/web.php
@@ -30,12 +30,13 @@ Route::middleware('auth')
         Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
         Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
         Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-        Route::resource('foods', FoodController::class);
     });
 
-/* Route::middleware(['auth', 'verified'])
+Route::middleware(['auth', 'verified'])
     ->name('admin.')
     ->prefix('admin')
-    ->group(function)  */
+    ->group(function () {
+        Route::resource('foods', FoodController::class);
+    });
 
 require __DIR__ . '/auth.php';
