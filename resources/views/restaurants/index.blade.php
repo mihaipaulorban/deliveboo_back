95b44aa6f3cae66593fe4636c7c85a3d8e848a31
DaviGel <geloso.davide@gmail.com>
2024-03-05 17:00:27 +0100

    fix: commentato codice inutilizzato

--- resources/views/restaurants/index.blade.php
+++ resources/views/restaurants/index.blade.php
@@ -7,20 +7,20 @@
     <ul>
         @foreach ($restaurants as $restaurant)
         <li class="list-unstyled">
-            <a href="{{ route('admin.restaurants.show', $restaurant) }}">
+            <a href="{{ route('admin.restaurants.index', $restaurant) }}">
                 <h2 class="mt-3">{{ $restaurant->name }}</h2>
             </a>
             <p>{{ $restaurant->address }}</p>
             <p>Partita IVA: {{ $restaurant->p_iva }}</p>
-            <form action="{{ route('admin.restaurants.destroy', $restaurant) }}" onsubmit="return confirm('Are you sure you want to delete this record?');" method="post">
+            {{-- <form action="{{ route('admin.restaurants.destroy', $restaurant) }}" onsubmit="return confirm('Are you sure you want to delete this record?');" method="post">
                 @csrf
                 @method('delete')
                 <button class="btn btn-danger">Delete this record</button>
                 <a class="btn btn-info" href="{{ route('admin.restaurants.edit', $restaurant) }}">Edit this record</a>
-            </form>
+            </form> --}}
         </li>
         @endforeach
     </ul>
-    <a class="btn btn-success mt-3" href="{{ route('admin.restaurants.create') }}">Add a new restaurant</a>
+    {{-- <a class="btn btn-success mt-3" href="{{ route('admin.restaurants.create') }}">Add a new restaurant</a> --}}
 </div>
 @endsection
