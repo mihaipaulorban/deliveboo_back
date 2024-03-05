231db8b2771c8d52da98b0ce0b5d7f7e2c4cea01
antoniodalterio <antoniodalterio2227@gmail.com>
2024-03-05 10:15:41 +0100

    feat: layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,17 +3,19 @@
 
 <div class="jumbotron p-5 mb-4  rounded-3">
     <div class="container  my-5 pr shadow p-3 mb-5 bg-body-tertiary rounded">
-        <div class="row">
-            <div class="col-6"> 
+        <div class="row flex-wrap">
+            <div class="col col-lg-6"> 
                 <h1 class="fw-bold text-primary">Welcome</h1>
 
-                <p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
+                <p class="fs-4 my-5">welcome to your restaurant management page.</p>
 
-                <button class="btn btn-primary btn-lg" type="button">Example button</button>
+                <button class="btn btn-primary btn-md" type="button">Example button</button>
             </div>
-            <div class="col-6">
+            <div class="col col-lg-6">
                 <div class="w-100">
-                    <img  src="https://cdn.discordapp.com/attachments/1213127782629113919/1213135927367434281/Immagini_organizzazione.png?ex=65f45fe0&is=65e1eae0&hm=69232915847dc5cea0adbfd9fdc42777bcf11e74251033713555a14fc622fdc6&" alt="">
+                    {{-- <img  class="img-fluid" src="https://cdn.discordapp.com/attachments/1213127782629113919/1213135927367434281/Immagini_organizzazione.png?ex=65f45fe0&is=65e1eae0&hm=69232915847dc5cea0adbfd9fdc42777bcf11e74251033713555a14fc622fdc6&" alt=""> --}}
+
+                    <div class="box w-100"></div>
                 </div>
             </div>
            
