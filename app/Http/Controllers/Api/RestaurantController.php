524abe75a536b48f39865321e22424adccd7c172
danielecarpentiero <carpentierodaniele@gmail.com>
2024-03-06 11:37:03 +0100

    feat: restaurant APIs

---
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Restaurant;
+use Illuminate\Http\Request;
+
+class RestaurantController extends Controller
+{
+    public function index()
+    {
+        $restaurants = Restaurant::with('foods', 'types')->paginate(20);
+        return response()->json([
+            'success' => true,
+            'results' => $restaurants
+        ]);
+    }
+}
