2c3327c9f7317f01144b6ad1f6fada436a084fbd
DaviGel <geloso.davide@gmail.com>
2024-03-06 12:00:31 +0100

    feat: immagini circolari in edit e show

--- resources/views/foods/edit.blade.php
+++ resources/views/foods/edit.blade.php
@@ -32,7 +32,8 @@
                 </label>
                 <div class="input-group mb-3">
                     <span class="input-group-text">€</span>
-                    <input type="text" class="form-control" name="price" aria-label="Amount (to the nearest euro)" value="{{ old('price', $food->price) }}">
+                    <input type="text" class="form-control" name="price" aria-label="Amount (to the nearest euro)"
+                        value="{{ old('price', $food->price) }}">
                 </div>
                 @error('price')
                     <div class="alert alert-danger">{{ $message }}</div>
@@ -43,13 +44,15 @@
             </div>
             <label for="is_visible" class="form-label mt-3">Can you see the item in your listing?</label>
             <div class="form-check">
-                <input class="form-check-input" type="radio" name="is_visible" id="is_visible1" value="1" {{ old('is_visible', $food->is_visible) === 1 ? 'checked' : '' }}>
+                <input class="form-check-input" type="radio" name="is_visible" id="is_visible1" value="1"
+                    {{ old('is_visible', $food->is_visible) === 1 ? 'checked' : '' }}>
                 <label class="form-check-label" for="is_visible1">
                     Visible
                 </label>
             </div>
             <div class="form-check">
-                <input class="form-check-input" type="radio" name="is_visible" id="is_visible2" value="0" {{ old('is_visible', $food->is_visible) === 0 ? 'checked' : '' }}>
+                <input class="form-check-input" type="radio" name="is_visible" id="is_visible2" value="0"
+                    {{ old('is_visible', $food->is_visible) === 0 ? 'checked' : '' }}>
                 <label class="form-check-label" for="is_visible2">
                     Not Visible
                 </label>
@@ -59,13 +62,15 @@
             </div>
             <label for="is_veggie" class="form-label mt-3">Is this dish vegetarian?</label>
             <div class="form-check">
-                <input class="form-check-input" type="radio" name="is_vegetarian" id="is_veggie1" value="1" {{ old('is_vegetarian', $food->is_vegetarian) === 1 ? 'checked' : '' }}>
+                <input class="form-check-input" type="radio" name="is_vegetarian" id="is_veggie1" value="1"
+                    {{ old('is_vegetarian', $food->is_vegetarian) === 1 ? 'checked' : '' }}>
                 <label class="form-check-label" for="is_veggie1">
                     Yes
                 </label>
             </div>
             <div class="form-check">
-                <input class="form-check-input" type="radio" name="is_vegetarian" id="is_veggie2" value="0" {{ old('is_vegetarian', $food->is_vegetarian) === 0  ? 'checked' : '' }}>
+                <input class="form-check-input" type="radio" name="is_vegetarian" id="is_veggie2" value="0"
+                    {{ old('is_vegetarian', $food->is_vegetarian) === 0 ? 'checked' : '' }}>
                 <label class="form-check-label" for="is_veggie2">
                     No
                 </label>
@@ -75,11 +80,15 @@
             </div>
             <div class="my-3">
                 <label for="img" class="form-label">Insert an image for the Dish</label>
-                <input class="form-control" type="file" id="img" name="img" value="{{ old('img', $food->img) }}">
-                @if($food->img)
+                <input class="form-control" type="file" id="img" name="img"
+                    value="{{ old('img', $food->img) }}">
+                @if ($food->img)
                     <div class="current-image mt-4">
                         <p>Currrent image:</p>
-                        <img style="width: 300px;" src="{{ asset('storage/' . $food->img) }}" alt="current-image">
+                        <div class="image-container">
+                            <img class="img-fluid circular-image" src="{{ asset('storage/' . $food->img) }}"
+                                alt="current-image">
+                        </div>
                     </div>
                 @endif
                 @error('img')
--- resources/views/foods/show.blade.php
+++ resources/views/foods/show.blade.php
@@ -1,20 +1,23 @@
 @extends('layouts.admin')
 
 @section('content')
-    
     <div class="container mt-4 vh-100">
         <h1> {{ $food->name }}</h1>
         @if ($food->is_vegetarian)
-        <span class="badge text-bg-success">Vegetarian</span>
-    @else
-        <span class="badge text-bg-warning">Non-Vegetarian</span>
-    @endif
+            <span class="badge text-bg-success mb-4">Vegetarian</span>
+        @else
+            <span class="badge text-bg-warning mb-2">Non-Vegetarian</span>
+        @endif
         <h2>Ingredienti</h2>
         <p>{{ $food->description }}</p>
-        <img src="{{ asset('storage/' . $food->img) }}" class="img-fluid img-thumbnail" alt="immagine del piatto">
-
-        <br>
-        <a class="btn btn-secondary" href="{{ route('admin.foods.index') }}">Back to foods list</a>
-        <a class="btn btn-info my-3 text-white" href="{{ route('admin.foods.edit', $food) }}">Edit this food</a>
+        @if ($food->img)
+            <div class="image-container">
+                <img src="{{ asset('storage/' . $food->img) }}" class="img-fluid circular-image" alt="immagine del piatto">
+            </div>
+        @endif
+        <div class="buttons mt-3 pb-3">
+            <a class="btn btn-secondary" href="{{ route('admin.foods.index') }}">Back to foods list</a>
+            <a class="btn btn-info text-white" href="{{ route('admin.foods.edit', $food) }}">Edit this food</a>
+        </div>
     </div>
 @endsection
