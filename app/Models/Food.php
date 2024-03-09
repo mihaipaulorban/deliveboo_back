03968de4f3fec1b864a9eb70df46421c914cb335
DaviGel <geloso.davide@gmail.com>
2024-03-09 22:41:14 +0100

    feat: relazione model food e order

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -19,4 +19,9 @@ class Food extends Model
     {
         return $this->belongsTo(Restaurant::class);
     }
+
+    public function orders()
+    {
+        return $this->belongsToMany(Order::class, 'foods_orders');
+    }
 }
