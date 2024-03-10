ef871433a2f43af7b2da06a471e78b99625f61ca
DaviGel <geloso.davide@gmail.com>
2024-03-10 18:47:26 +0100

    feat: orders controller

--- app/Http/Controllers/Admin/OrdersController.php
+++ app/Http/Controllers/Admin/OrdersController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Order;
+use Braintree\Gateway;
+// use App\Models\Order;
+// use Braintree\Configuration;
+// use Braintree\Transaction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -33,20 +37,56 @@ class OrdersController extends Controller
 
     public function store(Request $request)
     {
-        // Dati della carta con cui è stato effettuato l'ordine
-        // $nonce = $request->input('nonce');
+        // Inizializza il gateway Braintree utilizzando le credenziali dal file .env
+        $gateway = new Gateway([
+            'environment' => 'sandbox',
+            'merchantId' => 'n2sddky376q9g4bt',
+            'publicKey' => 'hzywfmfwm9cmgzv8',
+            'privateKey' => '0e0b1ed42edddab64a0b919b8d6de505'
+        ]);
 
-        $order = new Order();
-        $order->restaurant_id = $request->restaurant_id;
-        $order->guest_firstname = $request->guest_firstname;
-        $order->guest_surname = $request->guest_surname;
-        $order->guest_address = $request->guest_address;
-        $order->guest_phone = $request->guest_phone;
-        $order->email = $request->email;
-        $order->total = $request->total;
-        $order->save();
+        if ($request->input('nonce') != null) {
+            $nonce = $request->input('nonce');
+            $amount = $request->input('amount');
+            // $name = $request->input('guestName');
+            // $surname = $request->input('guestSurname');
+            // $address = $request->input('guestAddress');
+            // $phone = $request->input('guestPhone');
+            // $email = $request->input('guestEmail');
+
+            // Elabora la transazione di pagamento utilizzando il nonce e l'importo
+            $result = $gateway->transaction()->sale([
+                'amount' => $amount,
+                'paymentMethodNonce' => $nonce,
+                'options' => [
+                    'submitForSettlement' => True
+                ]
+            ]);
+
+            // Controlla se la transazione è stata eseguita con successo
+            if ($result->success) {
+                // $new_order = new Order();
+                // $new_order->name = $name;
+                // $new_order->surname = $surname;
+                // $new_order->address = $address;
+                // $new_order->phone = $phone;
+                // $new_order->email = $email;
+                // $new_order->save();
+
+                // La transazione è stata elaborata con successo
+                return response()->json([
+                    'message' => 'Transazione elaborata con successo',
+                    'result' => $result,
+                ], 200);
+            } else {
+                // Si è verificato un errore durante l'elaborazione della transazione
+                $errorMessages = $result->errors->deepAll();
+                return response()->json(['error' => $errorMessages[0]->message], 400);
+            }
+        }
 
         // Ritorna alla vista degli ordini dell'utente autenticato
-        return redirect()->route('orders.index');
+        // return redirect()->route('orders.index');
+        // return response()->json(['message' => 'Chiamata effettuata con successo'], 200);
     }
 }
