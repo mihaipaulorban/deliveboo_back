0f6924a2bf4d9df19abba26d3f6c85c8d90603b8
DaviGel <geloso.davide@gmail.com>
2024-03-12 20:07:56 +0100

    feat: dashboard style

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,7 +19,7 @@
         @endif
 
         {{-- Titolo --}}
-        <h2>Food items ({{count($foods)}})</h2>
+        <h2>Food items ({{ count($foods) }})</h2>
 
         {{-- Pulsante crea nuovo cibo --}}
         <a href="{{ route('admin.foods.create') }}" class="hoverable btn btn-success my-4">
@@ -113,7 +113,6 @@
                                     </div>
                                 </div>
                             </form>
-
                         </td>
                     </tr>
                 @endforeach
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -25,10 +25,11 @@
 
 <body>
     <div id="app">
-
-        <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-2 shadow">
+        {{-- Header --}}
+        {{-- <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-2 shadow">
             <div class="row justify-content-between">
                 <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/">DeliveBoo</a>
+                <!-- Bottone per il toggle della sidebar -->
                 <button class="navbar-toggler position-absolute d-md-none collapsed" type="button"
                     data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu"
                     aria-expanded="false" aria-label="Toggle navigation">
@@ -39,7 +40,65 @@
                 <div class="nav-item text-nowrap ms-2">
                     <a class="nav-link" href="{{ route('logout') }}"
                         onclick="event.preventDefault();
-                    document.getElementById('logout-form').submit();">
+                        document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </div>
+            </div>
+        </header>
+        {{-- Main --}}
+        {{-- <div class="container-fluid">
+            <div class="row">
+                <!-- Barra laterale -->
+                <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block sidebar collapse position-fixed">
+                    <div class="position-fixed pt-3">
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <a class="nav-link {{ Route::currentRouteName() == 'dashboard' ? 'bg-secondary' : '' }}"
+                                    href="{{ route('admin.foods.index') }}">
+                                    <i class="fa-solid fa-burger"></i> Foods
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white {{ Route::currentRouteName() == 'dashboard' ? 'bg-secondary' : '' }}"
+                                    href="{{ route('admin.restaurants.index') }}">
+                                    <i class="fa-solid fa-utensils"></i> Restaurants
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white {{ Route::currentRouteName() == 'dashboard' ? 'bg-secondary' : '' }}"
+                                    href="{{ route('admin.orders') }}">
+                                    <i class="fa-solid fa-receipt"></i> Orders
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                <main class="col-md-9 mx-sm-auto col-lg-9 px-md-4">
+                    @yield('content')
+                </main>
+            </div>
+        </div> --}}
+
+        {{-- Header --}}
+        <header class="navbar navbar-dark sticky-top flex-md-nowrap p-2 shadow">
+            <div class="row justify-content-between">
+                <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/">DeliveBoo</a>
+                <!-- Bottone per il toggle della sidebar -->
+                <button class="navbar-toggler position-absolute d-md-none collapsed" type="button"
+                    data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+            <div class="navbar-nav">
+                <div class="nav-item text-nowrap ms-2">
+                    <a class="nav-link" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();">
                         {{ __('Logout') }}
                     </a>
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
@@ -49,34 +108,34 @@
             </div>
         </header>
 
-        <div class="container-fluid vh-100">
-            <div class="row h-100">
-                <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block navbar sidebar">
+        <div class="container-fluid">
+            <div class="row">
+                <!-- Barra laterale -->
+                <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block sidebar collapse">
                     <div class="position-sticky pt-3">
                         <ul class="nav flex-column">
                             <li class="nav-item">
                                 <a class="nav-link {{ Route::currentRouteName() == 'dashboard' ? 'bg-secondary' : '' }}"
                                     href="{{ route('admin.foods.index') }}">
-                                    <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Foods
+                                    <i class="fa-solid fa-burger"></i> Foods
                                 </a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link text-white {{ Route::currentRouteName() == 'dashboard' ? 'bg-secondary' : '' }}"
                                     href="{{ route('admin.restaurants.index') }}">
-                                    <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Restaurants
+                                    <i class="fa-solid fa-utensils"></i> Restaurants
                                 </a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link text-white {{ Route::currentRouteName() == 'dashboard' ? 'bg-secondary' : '' }}"
                                     href="{{ route('admin.orders') }}">
-                                    <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Orders
+                                    <i class="fa-solid fa-receipt"></i> Orders
                                 </a>
                             </li>
                         </ul>
                     </div>
                 </nav>
-
-                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <main class="col-md-9 col-lg-10 px-md-5">
                     @yield('content')
                 </main>
             </div>
