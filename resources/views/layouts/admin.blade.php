15015fe1f0efc629391371aea8fa6a3c2c346235
DaviGel <geloso.davide@gmail.com>
2024-03-13 11:33:13 +0100

    feat: pagina orders

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -109,9 +109,9 @@
         </header>
 
         <div class="container-fluid">
-            <div class="row">
+            <div class="row overflow-auto">
                 <!-- Barra laterale -->
-                <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block sidebar collapse">
+                <nav id="sidebarMenu" class="d-md-block sidebar collapse">
                     <div class="position-sticky pt-3">
                         <ul class="nav flex-column">
                             <li class="nav-item">
--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -3,21 +3,67 @@
 @section('content')
     @include('partials.session_message')
     <div class="container">
-        <ul class="list-unstyled">
-            @foreach ($orders as $order)
-                <li>
-                    <h2 class="mt-4">{{ $order->guest_firstname }} {{ $order->guest_surname }}</h2>
-                    <p>Name: {{ $order->guest_firstname }}</p>
-                    <p>Surname: {{ $order->guest_surname }}</p>
-                </li>
-                <p>Nome del cibo: </p>
-                @foreach ($order->foods as $food)
-                    <li>
-                        {{ $food->name }}</p>
-                    </li>
-                @endforeach
-                <li></li>
-            @endforeach
-        </ul>
+        <div class="card mt-4 align-items-center shadow p-3 mb-5 bg-body-tertiary rounded border-0"
+            style="width: fit-content;">
+            <div class="card-body d-flex gap-3">
+                <h5 class="card-title mb-0">Total orders:</h5>
+                <p class="card-text">{{ count($orders) }}</p>
+            </div>
+        </div>
+        <div class="table-responsive-lg">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Id order:</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Surname</th>
+                        <th scope="col">Foods</th>
+                        <th scope="col">Data</th>
+                        <th scope="col">Total €</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($orders as $order)
+                        <tr>
+                            <th scope="row">{{ $order->id }}</th>
+                            <td>
+                                <p>{{ $order->guest_firstname }}</p>
+                            </td>
+                            <td>
+                                <p>{{ $order->guest_surname }}</p>
+                            </td>
+                            <td>
+                                @php
+                                    // Inizializza un array per memorizzare il conteggio totale delle occorrenze di ciascun cibo
+                                    $totalFoodCounts = [];
+
+                                    // Itera attraverso i cibi di ogni ordine per accumulare il conteggio totale delle occorrenze
+                                    foreach ($order->foods as $food) {
+                                        $foodName = $food->name;
+                                        if (isset($totalFoodCounts[$foodName])) {
+                                            // Incrementa il conteggio se il cibo è già stato incontrato prima
+                                            $totalFoodCounts[$foodName]++;
+                                        } else {
+                                            // Inizializza il conteggio a 1 se è la prima volta che il cibo viene incontrato
+                                            $totalFoodCounts[$foodName] = 1;
+                                        }
+                                    }
+                                @endphp
+
+                                @foreach ($totalFoodCounts as $foodName => $count)
+                                    <p>{{ $foodName }} (x{{ $count }})</p>
+                                @endforeach
+                            </td>
+                            <td>
+                                <p>{{ $order->created_at }}</p>
+                            </td>
+                            <td>
+                                <p>{{ $order->total }}</p>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 @endsection
