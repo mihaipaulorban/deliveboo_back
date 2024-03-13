eb24a9ab4b47bac4b73abe8d40855e0b32a7bfca
DaviGel <geloso.davide@gmail.com>
2024-03-13 16:25:38 +0100

    feat: img style modificato

--- resources/views/restaurants/index.blade.php
+++ resources/views/restaurants/index.blade.php
@@ -3,15 +3,17 @@
 @section('content')
     @include('partials.session_message')
     <div class="container">
-        <h2 class="mt-4">{{ $restaurant->name }}</h2>
-        <p>Address: {{ $restaurant->address }}</p>
-        <p>VAT Number: {{ $restaurant->p_iva }}</p>
-        <span>Categories:</span>
-        @foreach ($restaurant->types as $type)
-            <span>{{ $type->name }}</span>
-        @endforeach
-        <img src="{{asset('storage/' . $restaurant->cover_img)}}" alt="{{$restaurant->slug}}">
-        <img src="{{asset('storage/' . $restaurant->logo)}}" alt="{{$restaurant->slug}}">
-
+        <div class="info">
+            <h2 class="mt-4">{{ $restaurant->name }}</h2>
+            <p>Address: {{ $restaurant->address }}</p>
+            <p>VAT Number: {{ $restaurant->p_iva }}</p>
+            <span>Categories:</span>
+            @foreach ($restaurant->types as $type)
+                <span>{{ $type->name }}</span>
+            @endforeach
+        </div>
+        <div class="img">
+            <img id="restaurant-logo" src="{{ asset('storage/' . $restaurant->cover_img) }}" alt="{{ $restaurant->slug }}">
+        </div>
     </div>
 @endsection
