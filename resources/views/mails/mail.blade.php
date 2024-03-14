5c80baef66aac4facbc7aca99acbc54690f12bc5
lorenzo91347 <lorenzo.losito91@gmail.com>
2024-03-14 09:24:40 +0100

    feat:food detail in mails

--- resources/views/mails/mail.blade.php
+++ resources/views/mails/mail.blade.php
@@ -1,12 +1,15 @@
 <div class="container">
+    @php
+        $counts = array_count_values($foodNames);
+    @endphp
     <h1>Your Deliveboo Order</h1>
     <div>
         <p>Dear {{ $order->guest_firstname }} {{ $order->guest_surname }},</p>
         <p>Your order is on the way to you,please listen to your doorbell to receive your tasty food.</p>
         <p>Your food was sent to {{ $order->guest_address }}</p>
         <p>Your total was {{ $order->total }} Euros</p>
-        @foreach ($foodNames as $names)
-            <p>{{ $names }}</p>
+        @foreach ($counts as $item => $count)
+            Item {{ $item }} appears {{ $count }} times.<br>
         @endforeach
         <p>Enjoy your food!</p>
         <div>Sincerely</div>
