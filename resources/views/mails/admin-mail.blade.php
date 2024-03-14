a9ee2c4fc6dea9178b873a1bd6ca8f4981a09ef9
lorenzo91347 <lorenzo.losito91@gmail.com>
2024-03-14 16:03:55 +0100

    feat:mails modified

--- resources/views/mails/admin-mail.blade.php
+++ resources/views/mails/admin-mail.blade.php
@@ -1,8 +1,29 @@
 <div class="container">
-    <h2>Dear User</h2>
-    <div>A new order of {{ $order->total }} euros has ben sent to your restaurant.</div>
-    <div>your can check your orders in you Admin page on the website.</div>
-    <div>Sincerely</div>
-    <h3>THE DELIVEBOO TEAM</h3>
-
+    <div class="inner-container">
+        <h2>Dear User</h2>
+        <div>A new order of {{ $order->total }} € has ben sent to your restaurant.</div>
+        <div>your can check your orders in you Admin page on the website.</div>
+        <div>Sincerely</div>
+        <h3>THE DELIVEBOO TEAM</h3>
+    </div>
 </div>
+<style>
+    .container {
+        background-color: #386641;
+        padding: 20px 0;
+    }
+
+    .inner-container {
+
+        color: white;
+        width: 800px;
+        margin: auto;
+    }
+
+    .top {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+
+    }
+</style>
--- resources/views/mails/mail.blade.php
+++ resources/views/mails/mail.blade.php
@@ -1,24 +1,49 @@
+@php
+    $counts = array_count_values($foodNames);
+@endphp
+
+
+
 <div class="container">
-    @php
-        $counts = array_count_values($foodNames);
-    @endphp
-    <h1>Your Deliveboo Order</h1>
-    <div>
-        <p>Dear {{ $order->guest_firstname }} {{ $order->guest_surname }},</p>
-        <p>Your order is on the way to you,please listen to your doorbell to receive your tasty food.</p>
-        <p>Your food was sent to {{ $order->guest_address }}</p>
-        <p>Your total was {{ $order->total }} Euros</p>
-        {{-- @foreach ($counts as $item => $count)
-            Item {{ $item }} appears {{ $count }} times.<br>
-        @endforeach --}}
-        @foreach ($counts as $food => $count)
-            Item {{ $food }} appears {{ $count }} times.
-            @if (!$loop->last)
-                <br>
-            @endif
-        @endforeach
-        <p>Enjoy your food!</p>
-        <div>Sincerely</div>
-        <h3>THE DELIVEBOO TEAM</h3>
+    <div class="inner-container">
+        <div class="top">
+            <h1>Your Deliveboo Order</h1>
+        </div>
+        <div>
+            <p>Dear {{ $order->guest_firstname }} {{ $order->guest_surname }},</p>
+            <p>Your order is on the way to you,please listen to your doorbell to receive your tasty food.</p>
+            <p>Your food was sent to {{ $order->guest_address }}</p>
+            <div>Here is a summary of your order:</div>
+            @foreach ($counts as $food => $count)
+                <div> {{ $food }} - {{ $count }}x.</div>
+                @if (!$loop->last)
+                    <br>
+                @endif
+            @endforeach
+            <p>Total: {{ $order->total }} €</p>
+            <p>Enjoy your food!</p>
+            <div>Sincerely</div>
+            <h3>THE DELIVEBOO TEAM</h3>
+        </div>
     </div>
 </div>
+<style>
+    .container {
+        background-color: #386641;
+        padding: 20px 0;
+    }
+
+    .inner-container {
+
+        color: white;
+        width: 800px;
+        margin: auto;
+    }
+
+    .top {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+
+    }
+</style>
