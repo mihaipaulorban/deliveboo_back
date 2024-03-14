052a7ec4acabeb4912415efad422fa946168a1da
antoniodalterio <antoniodalterio2227@gmail.com>
2024-03-14 17:21:18 +0100

    feat: style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,14 +10,19 @@
 
             {{-- colonna 1 con testo --}}
 
-            <div class="col-12 col-md-6"> 
-                <h1 class="fw-bold text-primary">Welcome</h1>
+            <div class="col-12 text-center col-md-6"> 
+                <h1 class="fw-bold">Welcome</h1>
 
                 {{-- <div class="box-2"></div> --}}
 
                 <p class="fs-4 my-5">welcome to your restaurant management page.</p>
+                <div class="container p-3 mt-3">
+                <h3 >
+                    Welcome to our management website! Here you'll find valuable resources, effective tools, and innovative ideas to guide you towards success. We're here to support you on your journey of growth and professional development. Explore, learn, and be inspired. We're glad to have you with us!
+                </h3>
+            </div>
 
-                <button class="btn btn-primary btn-md" type="button">Example button</button>
+               
             </div>
             {{-- colonna 1 con testo --}}
 
@@ -25,7 +30,7 @@
             {{-- colonna 2 con img e position --}}
             <div class="col-12 col-md-6 pr ">
                 <div class="container-img">
-                    <img  class="img-fluid pr-img" src="https://cdn.discordapp.com/attachments/1213127782629113919/1213135927367434281/Immagini_organizzazione.png?ex=65f45fe0&is=65e1eae0&hm=69232915847dc5cea0adbfd9fdc42777bcf11e74251033713555a14fc622fdc6&" alt="">
+                    <img  class=" pr-img" src="https://cdn.discordapp.com/attachments/1213127782629113919/1213135927367434281/Immagini_organizzazione.png?ex=65f45fe0&is=65e1eae0&hm=69232915847dc5cea0adbfd9fdc42777bcf11e74251033713555a14fc622fdc6&" alt="">
 
                     {{-- <div class="box w-100"></div> --}}
                 </div>
@@ -39,9 +44,5 @@
     </div>
 </div>
 
-<div class="container p-3 mt-3">
-    <h3 class="text-primary">
-        Welcome to our management website! Here you'll find valuable resources, effective tools, and innovative ideas to guide you towards success. We're here to support you on your journey of growth and professional development. Explore, learn, and be inspired. We're glad to have you with us!
-    </h3>
-</div>
+
 @endsection
